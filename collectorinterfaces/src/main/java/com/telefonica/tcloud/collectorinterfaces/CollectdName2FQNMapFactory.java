058d236d901d33534e8e7fa62b8e9afde656b441
Chema Peribáñez <e.cloud3@tid.es>
2011-11-02 20:21:21 +0100

    factory to collectdname2fqn map

---
+++ collectorinterfaces/src/main/java/com/telefonica/tcloud/collectorinterfaces/CollectdName2FQNMapFactory.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.telefonica.tcloud.collectorinterfaces;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jomar
+ */
+public abstract class CollectdName2FQNMapFactory {
+    public abstract CollectdName2FQNMap getConversor(LinkedHashMap<String,String[]> config);
+    public static CollectdName2FQNMap getConversor(String factoryClassName,URL[] jarList,
+            LinkedHashMap<String,String[]> config) {
+        try {
+            
+            URLClassLoader classLoader=URLClassLoader.newInstance(jarList);
+            Class c=Class.forName(factoryClassName,
+                    true,classLoader);
+            String c1=c.getSuperclass().toString();
+            String c2=CollectdName2FQNMapFactory.class.toString();
+            
+            CollectdName2FQNMapFactory factory=Class.forName(factoryClassName,
+                    true,classLoader).asSubclass(
+                      CollectdName2FQNMapFactory.class).newInstance();
+            return factory.getConversor(config);
+        } catch (Exception ex) {
+            Logger.getLogger(MonPersistenceFactory.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    public static CollectdName2FQNMap getConversor(String factoryClassName,
+            LinkedHashMap<String,String[]> config) {
+        try {
+            CollectdName2FQNMapFactory factory=Class.forName(factoryClassName).asSubclass(
+                                    CollectdName2FQNMapFactory.class).newInstance();
+            return factory.getConversor(config);
+        } catch (Exception ex) {
+            Logger.getLogger(MonPersistenceFactory.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    
+}
---
+++ db_collectd2fqn/src/main/java/com/telefonica/tcloud/db_collectd2fqn/Collectd2FQNMapDBFactory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.telefonica.tcloud.db_collectd2fqn;
+import com.telefonica.tcloud.collectorinterfaces.CollectdName2FQNMap;
+import com.telefonica.tcloud.collectorinterfaces.CollectdName2FQNMapFactory;
+import java.util.LinkedHashMap;
+/**
+ *
+ * @author jomar
+ */
+public class Collectd2FQNMapDBFactory extends CollectdName2FQNMapFactory {
+
+    @Override
+    public CollectdName2FQNMap getConversor(LinkedHashMap<String, String[]> config) {
+        Collectd2FQNMapDB conversor=new Collectd2FQNMapDB();
+        conversor.setConfig(config);
+        return conversor;
+    }
+    
+}
