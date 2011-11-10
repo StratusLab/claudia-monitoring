b3e51c1d7c2bca411091fedaa986ee679e5d66b4
Chema Peribáñez <e.cloud3@tid.es>
2011-11-10 04:41:24 +0100

    MySQLMonPersistenceFactory must return MySQLMonPersistence, not JDBCMonPersistence

--- mysql_monpersistence/src/main/java/com/telefonica/tcloud/mysql_monpersistence/MySQL_MonPersistenceFactory.java
+++ mysql_monpersistence/src/main/java/com/telefonica/tcloud/mysql_monpersistence/MySQL_MonPersistenceFactory.java
@@ -6,7 +6,6 @@ package com.telefonica.tcloud.mysql_monpersistence;
 
 import com.telefonica.tcloud.collectorinterfaces.MonPersistence;
 import com.telefonica.tcloud.collectorinterfaces.MonPersistenceFactory;
-import com.telefonica.tcloud.jdbc_monpersistence.JDBC_MonPersistence;
 import java.sql.SQLException;
 import java.util.LinkedHashMap;
 import java.util.logging.Level;
@@ -17,7 +16,6 @@ import java.util.logging.Logger;
  * @author jomar
  */
 public class MySQL_MonPersistenceFactory extends MonPersistenceFactory {
-    public static final String driver="com.mysql.jdbc.Driver";
     
     @Override
     public MonPersistence getPersistence(LinkedHashMap<String, String[]> config) {
@@ -26,9 +24,9 @@ public class MySQL_MonPersistenceFactory extends MonPersistenceFactory {
                     config.get("mysqluser")[0]+
                     config.get("mysqlpassword")[0]);
                     
-            return new JDBC_MonPersistence(config.get("mysqlurl")[0],
+            return new MySQL_MonPersistence(config.get("mysqlurl")[0],
                     config.get("mysqluser")[0],
-                    config.get("mysqlpassword")[0], driver);
+                    config.get("mysqlpassword")[0]);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(MySQL_MonPersistenceFactory.class.getName()).log(Level.SEVERE, null, ex);
             return null;
