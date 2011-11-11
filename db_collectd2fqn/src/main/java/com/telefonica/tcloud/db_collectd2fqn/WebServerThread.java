2b05c554183e81ce22ffaae3725292dd3c909f08
Chema Peribáñez <e.cloud3@tid.es>
2011-11-11 17:10:36 +0100

    fixed loading of jars defining a parent class loader

--- db_collectd2fqn/src/main/java/com/telefonica/tcloud/db_collectd2fqn/WebServerThread.java
+++ db_collectd2fqn/src/main/java/com/telefonica/tcloud/db_collectd2fqn/WebServerThread.java
@@ -4,9 +4,11 @@
  */
 package com.telefonica.tcloud.db_collectd2fqn;
 
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
@@ -25,7 +27,13 @@ class WebServerThread extends Thread {
         WebAppContext webapp = new WebAppContext();
         webapp.setWar(warPath);
         webapp.setContextPath(warContext);
+        try {
+            webapp.setClassLoader(new WebAppClassLoader(this.getClass().getClassLoader(),webapp));
+        } catch (IOException ex) {
+            Logger.getLogger(WebServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
         server.setHandler(webapp);
+        
         try {
             server.start();
         } catch (Exception ex) {
