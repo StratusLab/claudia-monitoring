f7bd7cdfce0f665711b18a35ec7b555a71e22939
Chema Peribáñez <e.cloud3@tid.es>
2011-11-11 17:01:00 +0100

    added clean before making the package.

--- support/updaterpms.sh
+++ support/updaterpms.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)/..
 mvn clean; mvn install || exit 
-cd extended-probes-rpm ; mvn install || exit
-cd ../collectd-collector-rpm ; mvn install 
+cd extended-probes-rpm ; mvn clean; mvn install || exit
+cd ../collectd-collector-rpm ; mvn clean;  mvn install 
--- support/updaterpms_fast.sh
+++ support/updaterpms_fast.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)/..
 mvn clean; mvn install -Dmaven.test.skip=true || exit 
-cd extended-probes-rpm ; mvn -Dmaven.test.skip=true install || exit
-cd ../collectd-collector-rpm ; mvn -Dmaven.test.skip=true install 
+cd extended-probes-rpm ; mvn clean ;  mvn -Dmaven.test.skip=true install || exit
+cd ../collectd-collector-rpm ; mvn clean;  mvn -Dmaven.test.skip=true install 
