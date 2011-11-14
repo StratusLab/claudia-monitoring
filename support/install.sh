fdd09cf865fd96d0f58412b55d1faec6a60cc795
Chema Peribáñez <e.cloud3@tid.es>
2011-11-14 08:52:00 +0100

    moved collectd-rpm inside support, because is not part of code to integrate

--- support/install.sh
+++ support/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd $(dirname $0)
-sudo rpm -U ../collectd-rpm/RPMS/*/*.rpm
+sudo rpm -U collectd-rpm/RPMS/*/*.rpm
 sudo rpm -U ../collectd-extendedprobes-rpm/target/rpm/collectd-extendedprobes-rpm/RPMS/noarch/*.rpm
 sudo rpm -U ../collectd-collector-rpm/target/rpm/collectd-collector-rpm/RPMS/noarch/*.rpm
