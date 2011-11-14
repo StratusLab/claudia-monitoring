86f3e65a97eef56169960c97633b03aac3587283
Chema Peribáñez <e.cloud3@tid.es>
2011-11-14 13:30:25 +0100

    fix pathdir

--- support/build_collectd_rpm.sh
+++ support/build_collectd_rpm.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd $(dirname $0)
 test  ! -n "$JAVA_HOME" && echo 'JAVA_HOME must be defined' && exit
-#cd ../SOURCES ; wget http://www.collectd.org/files/collectd-5.0.1.tar.gz
+cd collectd-rpm
+cd SOURCES ; wget http://www.collectd.org/files/collectd-5.0.1.tar.gz
 cd ../SPECS
 rpmbuild -ba --define "_topdir $(pwd)/.." collectd.spec 
 
