b8719c6ee874e3554dc93fe610bf1d58c85e81e8
Chema Peribáñez <e.cloud3@tid.es>
2011-11-15 20:27:16 +0100

    add progress message to ensure that uninstall/install is done

--- support/fullreinstall.sh
+++ support/fullreinstall.sh
@@ -3,5 +3,7 @@ set -e
 cd $(dirname $0)
 ./updaterpms.sh
 cd $(dirname $0)
+echo "Uninstalling..."
 ./uninstall.sh
+echo "Installing..."
 ./install.sh
--- support/fullreinstall_fast.sh
+++ support/fullreinstall_fast.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 set -e
+echo "Building..."
 cd $(dirname $0)
 ./updaterpms_fast.sh
 cd $(dirname $0)
+echo "Uninstalling..."
 ./uninstall.sh
+echo "Installing..."
 ./install.sh
