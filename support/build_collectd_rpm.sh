75aa3a206332ea84890dc54c4e5faca25336c44a
Chema Peribáñez <jomar@chemacentoslaptop.(none)>
2012-04-17 19:30:53 +0200

    integrated memcached keyvaluecache in RPM

--- support/build_collectd_rpm.sh
+++ support/build_collectd_rpm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd $(dirname $0)
 test  ! -n "$JAVA_HOME" && echo 'JAVA_HOME must be defined' && exit
+test  ! $(id -u) -eq 0 && echo 'Please, invoke this script as root user' && exit
 yum install python-devel perl-ExtUtils-MakeMaker rpm-build
 cd collectd-rpm
 cd SOURCES ; wget http://www.collectd.org/files/collectd-5.0.1.tar.gz
